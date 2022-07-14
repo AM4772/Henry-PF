6a1a1fe92adf54ef8dff27f875d8615b5003e08e
AM4772 <moro_bramanti@hotmail.com>
2022-07-13 22:28:08 -0300

    initial commit

--- api/src/models/Books.js
+++ api/src/models/Books.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('country', {
+  sequelize.define('books', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Henry Countries</h1>
+      <h1>Henry BookStore</h1>
     </div>
   );
 }

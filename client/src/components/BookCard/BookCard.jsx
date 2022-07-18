ea2ee6350f46c1aa133acf20423b5a37c1a0b326
Esteban Manrupe <esteban_manrupe@hotmail.com>
2022-07-18 18:38:53 -0300

    feature/component/bookdetail+indexIcon+others

--- client/src/components/BookCard/BookCard.jsx
+++ client/src/components/BookCard/BookCard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import s from "./BookCard.module.sass";
+import { TESTING_BOOKS as books } from "../../testingObjects";
 
 function BookCard(props) {
   return <div></div>;

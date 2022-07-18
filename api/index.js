a629a0e89f9bdca6f51ab81a43779c2d6f6d11c4
AM4772 <moro_bramanti@hotmail.com>
2022-07-18 19:29:38 -0300

    modified api/index.js to load db

--- api/index.js
+++ api/index.js
@@ -1,32 +1,13 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require('./src/app.js');
-// const { dbLoad } = require('./src/controllers/XXXXXXX.js');
+const { getBooksApi } = require('./src/controllers/BooksControllers.js');
 const { conn } = require('./src/db.js');
 
 const PORT = process.env.PORT || '3001'; 
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  server.listen(PORT, () => {
+conn.sync({ force: true }).then(() => { 
+  server.listen(PORT, async () => {
+    await getBooksApi();
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-    // await dbLoad();
   });
 });

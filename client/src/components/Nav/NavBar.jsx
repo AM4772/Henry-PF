3a3a894f278c40dd0bdae59a7a0814431a0c7fec
Bruno-Pizarro <pizarrobrunotrabajo8@gmail.com>
2022-07-19 20:44:17 -0300

    feature/nav/search/filter. fixign filters spacing and adding input.

--- client/src/components/Nav/NavBar.jsx
+++ client/src/components/Nav/NavBar.jsx
@@ -5,18 +5,19 @@ import UserMenu from "../UserMenu/UserMenu";
 import s from "./Nav.module.sass";
 
 export default function NavBar() {
+  const [input, setInput] = useState("");
   const [, setOpen] = useState(false);
   const [, setHome] = useState(/.+(?=\/home$).+/.test(window.location.href));
   return (
     <nav className={s.nav}>
-      <NavLinks open={setOpen} home={setHome} />
+      <NavLinks setInput={setInput} open={setOpen} home={setHome} />
       {/* <div
         className={`${s.contSearch} ${
           /.+(?=\/home$).+/.test(window.location.href) ? null : s.searchHome
         }`}
       >
     </div> */}
-      <SearchBar />
+      <SearchBar input={input} setInput={setInput} />
       <UserMenu />
     </nav>
   );

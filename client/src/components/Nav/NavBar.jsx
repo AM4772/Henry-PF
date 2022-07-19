726402dbec44f9863eb6af0c2fd5fa8a8cf15802
Bruno-Pizarro <pizarrobrunotrabajo8@gmail.com>
2022-07-18 21:12:56 -0300

    feature/nav/searchBar/searching/components:_basic_logic_of_nav_searchBar_and_searching_complete

--- client/src/components/Nav/NavBar.jsx
+++ client/src/components/Nav/NavBar.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import NavLinks from "./NavLinks.jsx";
+import UserMenu from "../UserMenu/UserMenu";
 import s from "./Nav.module.sass";
 
-function NavBar(props) {
-  return <div></div>;
+export default function NavBar() {
+  const [, setOpen] = useState(false);
+  const [, setHome] = useState(/.+(?=\/home$).+/.test(window.location.href));
+  return (
+    <nav className={s.nav}>
+      <NavLinks open={setOpen} home={setHome} />
+      {/* <div
+        className={`${s.contSearch} ${
+          /.+(?=\/home$).+/.test(window.location.href) ? null : s.searchHome
+        }`}
+      >
+    </div> */}
+      <SearchBar />
+      <UserMenu />
+    </nav>
+  );
 }
-
-export default NavBar;

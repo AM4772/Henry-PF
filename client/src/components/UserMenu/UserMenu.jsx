57f0ebbb27a77d981a1237a8709153614295923e
NachoLosa <nachho.losa@gmail.com>
2022-07-18 23:38:58 -0300

    Structure, !styles

--- client/src/components/UserMenu/UserMenu.jsx
+++ client/src/components/UserMenu/UserMenu.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import s from "./UserMenu.module.sass";
 
 function UserMenu(props) {
-  return <div></div>;
+	/*   const {isLogged} = AlgunEstado */
+	const isLogged = true;
+
+	return (
+		<div>
+			{isLogged ? (
+				<div className={s.userLinks}>
+					<Link to="/user/:ID">
+						<span className={s.links}>Harry{/*User icon*/}</span>
+					</Link>
+					<Link to="/">
+						<span className={s.links}>{/*Cart icon*/}Carrito</span>
+					</Link>
+				</div>
+			) : (
+				<div className={s.visitLinks}>
+					<Link to="/login">
+						<span className={s.visitText}>Ingresar</span>
+					</Link>
+					<Link to="/">
+						<span className={s.visitText}>Registrarse</span>
+					</Link>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default UserMenu;

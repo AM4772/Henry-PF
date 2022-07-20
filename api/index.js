52193701c1148ab5e6acc2fea96151d683977c83
Quique Garay <98557235+Quique40@users.noreply.github.com>
2022-07-20 11:39:05 -0300

    link_home

--- api/index.js
+++ api/index.js
@@ -1,14 +1,14 @@
-const server = require('./src/app.js');
-const { getBooksApi } = require('./src/controllers/BooksControllers.js');
-const { Books } = require('./src/db');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { getBooksApi } = require("./src/controllers/BooksControllers.js");
+const { Books } = require("./src/db");
+const { conn } = require("./src/db.js");
 
-const PORT = process.env.PORT || '3001';
+const PORT = process.env.PORT || "3001";
 
-conn.sync({ force: false }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, async () => {
     const booksDb = await Books.findAll();
     booksDb.length > 0 ? null : await getBooksApi();
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
 });
--- api/src/db.js
+++ api/src/db.js
@@ -1,21 +1,20 @@
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, NODE_ENV='production'
-} = process.env;
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, NODE_ENV = "" } = process.env;
+//NODE_ENV= PRODUCTION
 
 let sequelize =
-  NODE_ENV === 'production'
+  NODE_ENV === "production"
     ? new Sequelize({
-        database: 'd2qoblbukidquf',
-        dialect: 'postgres',
-        host: 'ec2-52-206-182-219.compute-1.amazonaws.com',
+        database: "d2qoblbukidquf",
+        dialect: "postgres",
+        host: "ec2-52-206-182-219.compute-1.amazonaws.com",
         port: 5432,
-        username: 'ualijjydbrwnjx',
+        username: "ualijjydbrwnjx",
         password:
-          '1ee05d86be2f20b4b5e00a15caa749eb75a27b28a9215129d4f4bb5c43a77366',
+          "1ee05d86be2f20b4b5e00a15caa749eb75a27b28a9215129d4f4bb5c43a77366",
         pool: {
           max: 3,
           min: 1,
@@ -40,17 +39,23 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach(model => model(sequelize));
+modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
@@ -62,5 +67,5 @@ const { Books, Users } = sequelize.models;
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
-  conn: sequelize,     // para importart la conexión { conn } = require('./db.js');
+  conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };

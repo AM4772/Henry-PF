b39ee0a39a5fbb83ea5d444fce3d9e7eb110e0dd
Quique Garay <98557235+Quique40@users.noreply.github.com>
2022-07-21 12:49:25 -0300

    revision_login

--- api/src/routes/login.js
+++ api/src/routes/login.js
@@ -3,14 +3,14 @@ const router = Router();
 
 const { verifyLogin } = require("../utils/verifyLogin/verifyUserLogin");
 
-router.post("/login", async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const validateLogin = verifyLogin(req.body);
     validateLogin
       ? res.status(201).json(validateLogin)
       : res.status(400).json("Wrong username or password");
   } catch (err) {
-    next(err.message);
+    res.status(400).json(err.message);
   }
 });
 

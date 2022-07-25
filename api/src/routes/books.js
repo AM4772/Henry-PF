6c252b55121ee20b9d0a42865b39338d76a03771
Mariano Ceballos Torres <nanoceballostorres@gmail.com>
2022-07-25 19:27:33 -0300

    addMessageToResponseFail and added best selleres, etc to home

--- api/src/routes/books.js
+++ api/src/routes/books.js
@@ -1,38 +1,46 @@
-const { Router } = require("express");
+const { Router } = require('express');
 const {
   getBookByTitle,
   getBookById,
   getBooks,
-} = require("../controllers/BooksControllers");
+  getHomeBooks,
+} = require('../controllers/BooksControllers');
 
 const router = Router();
 
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   const { title } = req.query;
   try {
     if (title) {
       let book = await getBookByTitle(title);
-      book ? res.json(book) : res.status(404).json("Book not found");
+      book ? res.json(book) : res.status(404).json({message:'Book not found'});
     } else {
       let dbBooks = await getBooks();
-      dbBooks ? res.json(dbBooks) : res.status(404).json("No books found");
+      dbBooks ? res.json(dbBooks) : res.status(404).json({message:'No books found'});
     }
   } catch (err) {
     res.status(404).json(err);
   }
 });
 
-router.get("/:ID", async (req, res) => {
+router.get('/homeBooks', async (req, res) => {
+  const books = await getHomeBooks();
+  books
+    ? res.send(books)
+    : res.status(404).send({ message: 'Cannot get books' });
+});
+
+router.get('/:ID', async (req, res) => {
   const { ID } = req.params;
 
   try {
     if (isNaN(ID)) {
-      return res.status(400).json("ID must be a number");
+      return res.status(400).json({message:'ID must be a number'});
     }
     let book = await getBookById(ID);
     book
       ? res.json(book)
-      : res.status(404).json("Book not found with id " + ID);
+      : res.status(404).json({message:'Book not found with id ' + ID});
   } catch (err) {
     res.status(404).json(err);
   }

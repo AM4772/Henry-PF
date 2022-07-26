aebb938969f80959319868b48a65c5eb19d35e11
Quique Garay <98557235+Quique40@users.noreply.github.com>
2022-07-26 15:36:40 -0300

    2da correction route usernames and emails

--- api/src/routes/usernames.js
+++ api/src/routes/usernames.js
@@ -5,9 +5,9 @@ const { getUsernames } = require("../controllers/UsersControllers");
 
 router.get("/", async (req, res) => {
   try {
-    let userNames = await getUsernames();
-    userNames
-      ? res.json(userNames)
+    let usernames = await getUsernames();
+    usernames
+      ? res.json(usernames)
       : res.status(404).json({ message: "Cannot get usernames" });
   } catch (err) {
     res.status(404).json(err);

789e088854aed31085522cdd1777c24331af2704
Quique Garay <98557235+Quique40@users.noreply.github.com>
2022-07-26 16:48:36 -0300

    route put/users

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,16 +1,17 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const {
   getUserByUsername,
   getUserById,
   createUser,
   getUsers,
-} = require('../controllers/UsersControllers');
+  modifyUsers,
+} = require("../controllers/UsersControllers");
 
-const { validateUsersPost } = require('../utils/validations/userValidations');
+const { validateUsersPost } = require("../utils/validations/userValidations");
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   const { username } = req.query;
 
   try {
@@ -23,19 +24,19 @@ router.get('/', async (req, res) => {
       let dbUsers = await getUsers();
       dbUsers
         ? res.json(dbUsers)
-        : res.status(404).json({ message: 'No users found' });
+        : res.status(404).json({ message: "No users found" });
     }
   } catch (err) {
     res.status(400).json(err);
   }
 });
 
-router.get('/:ID', async (req, res) => {
+router.get("/:ID", async (req, res) => {
   const { ID } = req.params;
   try {
     if (ID) {
       if (isNaN(ID)) {
-        return res.status(400).json({ message: 'ID must be a number' });
+        return res.status(400).json({ message: "ID must be a number" });
       }
       let user = await getUserById(ID);
       user
@@ -47,13 +48,13 @@ router.get('/:ID', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     const validate = await validateUsersPost(req.body);
     if (!validate) {
       const newUser = await createUser(req.body);
       newUser
-        ? res.status(201).json({ message: 'Successfully registered' })
+        ? res.status(201).json({ message: "Successfully registered" })
         : res.status(400).json({ message: `Error creating user` });
     } else {
       res.status(400).json(validate);
@@ -63,4 +64,17 @@ router.post('/', async (req, res) => {
   }
 });
 
+router.put("/:ID", async (req, res) => {
+  const { ID } = req.params;
+  try {
+    if (ID) {
+      const modified = await modifyUsers(req.body, ID);
+      modified
+        ? res.status(200).json({ message: "User modified successfully" })
+        : res.status(400).json({ message: `Error modify user` });
+    }
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
+});
 module.exports = router;

079553465579aa557666f020cadf77b6eaa332f4
Esteban Manrupe <esteban_manrupe@hotmail.com>
2022-07-31 20:57:19 -0300

    feature/various19

--- client/src/components/BookCard/BookCard.jsx
+++ client/src/components/BookCard/BookCard.jsx
@@ -1,10 +1,59 @@
-import React from "react";
+import React, { useEffect } from "react";
 import s from "./BookCard.module.sass";
 import Loading from "../Loading/Loading";
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import Swal from "sweetalert2";
+import {
+  asyncAddFavourite,
+  asyncDeleteFavourite,
+} from "../../redux/actions/usersActions";
+import heartOff from "../../assets/Heart_off.png";
+import heartOn from "../../assets/Heart_on.png";
 
 function BookCard(props) {
   let book = props;
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { favourites } = useSelector((state) => state.profile);
+  const { userProfile } = useSelector((state) => state.profile);
+  const [addedBook, setAddedBook] = useState(false);
+
+  useEffect(() => {
+    if (favourites.length) {
+      let result = favourites.find((el) => el.ID === parseInt(book.ID));
+      if (result) setAddedBook(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [favourites]);
+
+  function addingFav() {
+    if (!userProfile.ID) {
+      Swal.fire({
+        title: "To add a favourite book, you have to be logged in",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Go to Login",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          history.push("/login");
+        }
+      });
+    }
+    if (userProfile.ID && !addedBook) {
+      dispatch(asyncAddFavourite(userProfile.ID, book.ID));
+      setAddedBook(true);
+    }
+    if (userProfile.ID && addedBook) {
+      dispatch(asyncDeleteFavourite(userProfile.ID, book.ID));
+      setAddedBook(false);
+    }
+  }
+
   return (
     <div className={s.cards}>
       {book.title ? (
@@ -37,14 +86,25 @@ function BookCard(props) {
                 </p>
                 {/* <p id={s.author}>{book.authors}</p> */}
               </div>
-              <div className={s.price}>
-                <p>
-                  $
-                  {new Intl.NumberFormat("es-ES", {
-                    maximumFractionDigits: 2,
-                    minimumFractionDigits: 2,
-                  }).format(book.price)}
-                </p>
+              <div className={s.containerheart}>
+                <div>
+                  <img
+                    className={s.imgHeart}
+                    alt="heart"
+                    title={!addedBook ? "Add Favourite" : "Delete Favourite"}
+                    src={!addedBook ? heartOff : heartOn}
+                    onClick={addingFav}
+                  />
+                </div>
+                <div className={s.price}>
+                  <p>
+                    $
+                    {new Intl.NumberFormat("es-ES", {
+                      maximumFractionDigits: 2,
+                      minimumFractionDigits: 2,
+                    }).format(book.price)}
+                  </p>
+                </div>
               </div>
             </div>
           </NavLink>
--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { NavLink, useHistory } from 'react-router-dom';
-import Loading from '../Loading/Loading.jsx';
-import CartCard from '../CartCard/CartCard.jsx';
-import { useDispatch } from 'react-redux';
-import { asyncGetItemsCart } from '../../redux/actions/usersActions';
-import s from './Cart.module.sass';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { NavLink, useHistory } from "react-router-dom";
+import Loading from "../Loading/Loading.jsx";
+import CartCard from "../CartCard/CartCard.jsx";
+import { useDispatch } from "react-redux";
+import { asyncGetItemsCart } from "../../redux/actions/usersActions";
+import s from "./Cart.module.sass";
 
 function Cart() {
   const [loading, setLoading] = useState(true);
   const dispatch = useDispatch();
   const history = useHistory();
-  const { cart, userProfile } = useSelector(state => state.profile);
-  const { stack } = useSelector(state => state.history);
+  const { cart, userProfile } = useSelector((state) => state.profile);
+  const { stack } = useSelector((state) => state.history);
 
   useEffect(() => {
     if (!cart.length && userProfile.ID) {
@@ -32,9 +32,9 @@ function Cart() {
     var lastPath = [];
     for (let i = 1; i < stack.length; i++) {
       if (
-        stack[i] !== '/register' &&
-        stack[i] !== '/login' &&
-        stack[i] !== '/profile' &&
+        stack[i] !== "/register" &&
+        stack[i] !== "/login" &&
+        stack[i] !== "/profile" &&
         stack[i] !== stack[0]
       ) {
         lastPath.push(stack[i]);
@@ -43,7 +43,7 @@ function Cart() {
     if (lastPath.length > 0) {
       history.push(lastPath[0]);
     } else {
-      history.push('/');
+      history.push("/");
     }
   };
   return cart.length ? (
@@ -53,7 +53,7 @@ function Cart() {
           Back
         </button>
       </div>
-      {cart.map(b => (
+      {cart.map((b) => (
         <div key={b.ID} className={s.containerFav1}>
           <CartCard
             key={b.ID}
@@ -67,34 +67,34 @@ function Cart() {
         </div>
       ))}
       <div id={s.centerMe}>
-        <NavLink to="/" className="buttons">
+        <NavLink to="/" className={s.buttonsCart}>
           CONTINUE BUYING
         </NavLink>
-        <button className="buttons">BUY</button>
+        <button className={s.buttonsCart}>BUY</button>
       </div>
     </div>
   ) : loading && !cart.length ? (
     <Loading />
   ) : (
-  <div className={s.containerNotFav0}>
-    <div className={s.containerNotFav}>
-      <div className={s.backButton}>
-        <button className={s.buttonBack} onClick={goBack}>
-          Back
-        </button>
-      </div>
-      <div className={s.NotFav}>
-        <h4>You have not added any book to your cart yet</h4>
-        <p>
-          You can add books to your cart by clicking on the <b>add to cart</b>{' '}
-          button that appears in the detail of each book.
-        </p>
-        <p>
-          Browse the <a href="/">home</a> section to look for books 🤠!
-        </p>
+    <div className={s.containerNotFav0}>
+      <div className={s.containerNotFav}>
+        <div className={s.backButton}>
+          <button className={s.buttonBack} onClick={goBack}>
+            Back
+          </button>
+        </div>
+        <div className={s.NotFav}>
+          <h4>You have not added any book to your cart yet</h4>
+          <p>
+            You can add books to your cart by clicking on the <b>add to cart</b>{" "}
+            button that appears in the detail of each book.
+          </p>
+          <p>
+            Browse the <a href="/">home</a> section to look for books 🤠!
+          </p>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 

ac804201f0e8416c71aeabc97e78c2abbae2139d
ClaraArguello <80827654+ClaraArguello@users.noreply.github.com>
2022-08-08 16:54:30 -0300

    feature/component/forgotPassword almost finished

--- client/src/components/ForgotPassword/ConfirmCode.jsx
+++ client/src/components/ForgotPassword/ConfirmCode.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import s from "./ForgotPassword.module.sass";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -7,33 +7,74 @@ function ConfirmCode() {
   const dispatch = useDispatch();
   const history = useHistory();
 
-  const [code, setCode] = useState("");
+  const isValidInitialState = {
+    code: "",
+  };
 
-  //   const handleButton = () => {
-  //     if (!isPending && isAllowed && refresh !== 1)
-  //       return (
-  //         <button className={`buttons ${s.login}`} id={s.active}>
-  //           Log In
-  //         </button>
-  //       );
-  //     else if (isPending)
-  //       return (
-  //         <p className="buttons" id={s.waiting}>
-  //           Logging In...
-  //         </p>
-  //       );
-  //     else
-  //       return (
-  //         <p className="buttons" id={s.waiting}>
-  //           Log In
-  //         </p>
-  //       );
-  //   };
+  /////////////STATE???
+
+  const [code, setCode] = useState("");
+  const [isValid, setIsvalid] = useState(isValidInitialState);
+  const [isPending, setIsPending] = useState(false);
+  const [refresh, setRefresh] = useState(0);
+  const [isAllowed, setIsAllowed] = useState(false);
+
+  useEffect(() => {
+    const isValidCopy = { ...isValid };
+    if (!code.length) isValidCopy.code = " ";
+    else if (code) {
+      isValidCopy.code = "Reset code is invalid";
+    } else delete isValidCopy.code;
+    setIsvalid(isValidCopy);
+    let counter = 0;
+    for (let err in isValidCopy) {
+      if (isValidCopy[err]) counter++;
+    }
+    if (!counter) setIsAllowed(true);
+    else if (counter) setIsAllowed(false);
+  }, [code]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsPending(true);
+    ////////////////DISPATCH
+    dispatch(code).then((res) => {
+      if (res) {
+        /////////////////////RUTA
+        history.push("/resetPassword");
+        setCode("");
+        setIsvalid(isValidInitialState);
+        setIsAllowed(false);
+        setIsPending(false);
+      } else setIsPending(false);
+    });
+  };
+
+  const handleButton = () => {
+    if (!isPending && isAllowed && refresh !== 1)
+      return (
+        <button className={`buttons ${s.login}`} id={s.active}>
+          Confirm code
+        </button>
+      );
+    else if (isPending)
+      return (
+        <p className="buttons" id={s.waiting}>
+          Confirming code...
+        </p>
+      );
+    else
+      return (
+        <p className="buttons" id={s.waiting}>
+          Confirm code
+        </p>
+      );
+  };
 
   return (
     <div className={s.forgotPsw}>
       <div className={s.card}>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <h1 className={s.title}>Confirm your code</h1>
           <div className={s.info}>
             <label className={s.mail}>Confirmation code:</label>
@@ -42,9 +83,9 @@ function ConfirmCode() {
               type="text"
               value={code}
               placeholder="Confirmation code"
-              //   onChange={(e) => setEmailOrUsername(e.target.value)}
+              onChange={(e) => setCode(e.target.value)}
             />
-            {/* <div id="button-handler">{handleButton()}</div> */}
+            <div id="button-handler">{handleButton()}</div>
           </div>
         </form>
       </div>

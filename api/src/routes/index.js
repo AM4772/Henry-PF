4868349f6bdbb387d19c65e417f6f75c0128e28f
Quique Garay <98557235+Quique40@users.noreply.github.com>
2022-08-09 10:17:58 -0300

    Routes Stats

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,19 +1,20 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
-router.use('/books', require('./books'));
-router.use('/users', require('./users'));
-router.use('/usernames', require('./usernames'));
-router.use('/emails', require('./emails'));
-router.use('/authors', require('./authors'));
-router.use('/categories', require('./categories'));
-router.use('/login', require('./login'));
-router.use('/reviews', require('./reviews'));
-router.use('/payments', require('./payments'));
-router.use('/confirm', require('./confirm'));
-router.get('/', (req, res) => {
+router.use("/books", require("./books"));
+router.use("/users", require("./users"));
+router.use("/usernames", require("./usernames"));
+router.use("/emails", require("./emails"));
+router.use("/authors", require("./authors"));
+router.use("/categories", require("./categories"));
+router.use("/login", require("./login"));
+router.use("/reviews", require("./reviews"));
+router.use("/payments", require("./payments"));
+router.use("/confirm", require("./confirm"));
+router.use("/stats", require("./stats"));
+router.get("/", (req, res) => {
   res.send(
-    '<div>All books: <a href=/books>/books</a></div> <div>Test books query: <a href=/books?title=hobbit>/books?title=hobbit</a></div> <div> Test books by ID: <a href=/books/10 > /books/10</a></div> <div>All users: <a href=/users>/users</a></div><div> Test user query: <a href=/users?username=test>/user?username=test</a></div><div> Test users by ID: <a href=/users/1 > /users/1</a></div> <div> Test username: <a href=/usernames> /usernames</a></div> <div> Test emails: <a href=/emails> /emails</a></div><div> All authors: <a href=/authors>/authors</a></div> <div> All categories: <a href=/categories>/categories</a></div>'
+    "<div>All books: <a href=/books>/books</a></div> <div>Test books query: <a href=/books?title=hobbit>/books?title=hobbit</a></div> <div> Test books by ID: <a href=/books/10 > /books/10</a></div> <div>All users: <a href=/users>/users</a></div><div> Test user query: <a href=/users?username=test>/user?username=test</a></div><div> Test users by ID: <a href=/users/1 > /users/1</a></div> <div> Test username: <a href=/usernames> /usernames</a></div> <div> Test emails: <a href=/emails> /emails</a></div><div> All authors: <a href=/authors>/authors</a></div> <div> All categories: <a href=/categories>/categories</a></div>"
   );
 });
 

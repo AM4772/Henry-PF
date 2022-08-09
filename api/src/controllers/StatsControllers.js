ad2752c9234adb50d13cb7884f3db2f476734e26
Mariano <nanoceballostorres@gmail.com>
2022-08-09 11:28:20 -0300

    changes

--- api/src/controllers/StatsControllers.js
+++ api/src/controllers/StatsControllers.js
@@ -1,12 +1,52 @@
-const { Categories, Books } = require('../db');
+const { Categories, Books, Users, Payments } = require('../db');
 let statsModel = {
   categoriesStats: async function () {
-    const categoryStats = Categories.findAll();
-    console.log(categoryStats);
+    const catArray = [];
+    const categoryStats = await Categories.findAll({
+      attributes: ['ID', 'category', 'soldCopies'],
+    });
+    for (let i = 0; i < categoryStats.length; i++) {
+      const category = categoryStats[i].toJSON();
+      if (category.soldCopies > 0) catArray.push(category);
+    }
+    return catArray;
   },
-  categoriesStats: async function () {
-    const bookStats = Books.findAll();
-    console.log(bookStats);
+
+  bookStats: async function () {
+    const bookArray = [];
+    const bookStats = await Books.findAll({
+      attributes: ['ID', 'title', 'image', 'soldCopies'],
+    });
+
+    for (let i = 0; i < bookStats.length; i++) {
+      const book = bookStats[i].toJSON();
+      if (book.soldCopies > 0) bookArray.push(book);
+    }
+
+    return bookArray;
+  },
+
+  paymentsStats: async function () {
+    const paymentStat = await Payments.findAll({
+      attributes: ['items', 'createdAt', 'total'],
+    });
+
+    return paymentStat.map((p) => {
+      return {
+        ...p.toJSON(),
+        items: p.toJSON().items.length,
+      };
+    });
+  },
+
+  userStats: async function () {
+    const userStat = await Users.findAll({
+      attributes: ['createdAt'],
+    });
+    return {
+      totalUsers: userStat.length,
+      createdDate: userStat,
+    };
   },
 };
 

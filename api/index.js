1a54008ccf453316e2cadb9a9fc80e0b43cc9732
TomasBorquez <tomas@affable.ai>
2023-07-02 15:29:05 -0300

    refactor startup

--- api/index.js
+++ api/index.js
@@ -5,47 +5,38 @@ const { booksWithImg } = require('./src/controllers/BooksWithLargeImage.js');
 const {
   fillCategories,
 } = require('./src/controllers/CategoriesControllers.js');
-const { Apibooks, Books, Categories, Authors } = require('./src/db');
+const { SetupStatus } = require('./src/db');
 const { conn } = require('./src/db.js');
 
 const PORT = process.env.PORT || '3001';
+const HOST = process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost';
 
 conn.sync({ force: false }).then(() => {
-  server.listen(PORT, async () => {
-    const apiBooksDb = await Apibooks.findAll();
-    const booksDb = await Books.findAll();
-    const categoriesDb = await Categories.findAll();
-    const authorsDb = await Authors.findAll();
+  server.listen(PORT, HOST, async () => {
+    const setupStatus = await SetupStatus.findOne({ where: { id: 1 } });
 
-    if (apiBooksDb.length > 0) {
-      console.log('Table ApiBooks already with books, nothing to add');
-    } else {
+    if (!setupStatus || !setupStatus.initialSetupDone) {
       console.log('Filling ApiBooks table...');
       await getBooksApi();
-      console.log('Done');
-    }
-    if (booksDb.length > 0) {
-      console.log('Table Books already with books, nothing to add');
-    } else {
+      console.log('Done ✅');
+
       console.log('Filling Books table...');
       await booksWithImg();
-      console.log('Done');
-    }
-    if (categoriesDb.length > 0) {
-      console.log('Table Categories already with data, nothing to add');
-    } else {
+      console.log('Done ✅');
+
       console.log('Filling Categories table...');
       await fillCategories();
-      console.log('Done');
-    }
-    if (authorsDb.length > 0) {
-      console.log('Table Authors already with data, nothing to add');
-    } else {
+      console.log('Done ✅');
+
       console.log('Filling Authors table...');
       await fillAuthors();
-      console.log('Done');
+      console.log('Done ✅');
+
+      await SetupStatus.upsert({ id: 1, initialSetupDone: true });
+    } else {
+      console.log('Initial setup already done, skipping...');
     }
 
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+    console.log(`Listening at ${PORT} 🤠`);
   });
 });

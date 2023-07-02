aae40eba7eb38dfa8e14bd595c8f21b16b26832a
Tomas Borquez <tomasborquez13@gmail.com>
2023-07-02 16:00:44 -0300

    Update cors :P

--- api/src/app.js
+++ api/src/app.js
@@ -12,6 +12,7 @@ server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+server.use(cors())
 server.use((req, res, next) => {
   const allowedOrigins = [local, vercel];
   const origin = req.headers.origin;
